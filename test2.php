0a53bb099553e558ccee261ad56b2c8db66213c7
Arun Abraham <arunnabraham@gmail.com>
2022-12-12 01:06:21 +0530

    server codes

--- test2.php
+++ test2.php
@@ -1,10 +1,10 @@
 <?php
 
 header('Content-Type: application/json');
-$retryInfoPath = __DIR__ . '/retries.txt';
+$retryInfoPath = __DIR__ . '/retries2.txt';
 $retries = (int)file_get_contents($retryInfoPath);
 file_put_contents($retryInfoPath, (string)($retries+1));
-if($retries >= 5)
+if($retries >= 2)
 {
     http_response_code(200);
     echo json_encode([
@@ -18,4 +18,3 @@ echo json_encode([
     "status" => "Server Error"
 ]);
 }
-
