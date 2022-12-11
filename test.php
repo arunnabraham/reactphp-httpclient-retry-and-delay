e517b156f1136570d1304516aa10d85f6dc7580e
Arun Abraham <arunnabraham@gmail.com>
2022-12-12 01:04:22 +0530

    Add files via upload
    Main source

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+
+use ArunabrahamPup\HttpRetryWithDelay\Http\Client;
+use Psr\Http\Message\ResponseInterface;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+$client = new Client;
+
+$q = new Clue\React\Mq\Queue(3, null, function ($url) use ($client) {
+    return $client->get($url);
+});
+
+$urls = [
+    'http://localhost:4000/test.php',
+    'http://localhost:4000/test2.php'
+];
+foreach ($urls as $url) {
+    $q($url)->then(function (ResponseInterface $response) use ($url) {
+        echo json_encode([
+            'url' => $url,
+            'statusCode' => $response->getStatusCode(),
+            'body' =>  $response->getBody()->getContents()
+        ]) . PHP_EOL;
+    });
+}
